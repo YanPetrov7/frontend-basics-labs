57dc79330f334c670db46d56ed635e3de0b941ab
Yan Petrov <yanemerald2004@gmail.com>
2023-11-16 12:44:36 +0200

    lab4: complete second task

--- lab4/script.js
+++ lab4/script.js
@@ -18,3 +18,43 @@ thirdElement.addEventListener("click", () => {
 fourthElement.addEventListener("click", () => {
   changeColor(fourthElement)
 });
+
+// task2
+const picture = document.getElementsByClassName("picture")[0];
+const imageContainer = document.getElementById("imageContainer");
+
+const addButton = document.getElementById("add");
+const enlargeButton = document.getElementById("enlarge");
+const reduceButton = document.getElementById("reduce");
+const deleteButton = document.getElementById("delete");
+
+// Fucntion to adjust the width of all images of the class "picture"
+const adjustWidth = (amount) => {
+  const images = document.querySelectorAll(".picture");
+  for (const image of images) {
+    const currentWidth = parseInt(getComputedStyle(image).getPropertyValue('width'));
+    image.style.width = `${currentWidth + amount}px`;
+  }
+};
+
+addButton.addEventListener("click", () => {
+  const clonedImage = document.querySelector(".picture").cloneNode(true);
+  
+  imageContainer.appendChild(clonedImage);
+});
+
+enlargeButton.addEventListener("click", () => {
+  adjustWidth(50);
+});
+
+reduceButton.addEventListener("click", () => {
+  adjustWidth(-50);
+});
+
+deleteButton.addEventListener("click", () => {
+  const images = document.querySelectorAll(".picture");
+  if (images.length > 1) {
+    const lastImage = images[images.length - 1];
+    lastImage.remove();
+  }
+});
